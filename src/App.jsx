85a599327dd9bab10a0e435d66391e2f88dfbee7
faizan-20 <faizan.20a@gmail.com>
2023-09-28 22:44:38 +0530

    added home page and routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css'
 import Navbar from './components/Navbar';
 import RoboCity from './components/RoboCity';
+import Home from './components/Home';
 
 function App() {
 
   return (
     <>
       <Navbar/>
-      <RoboCity/>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/robo-city' element={<RoboCity/>} />
+      </Routes>
     </>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
+
 function Home() {
     return (
-        <div>
-
+        <div className=" flex justify-center items-center h-[90vh] text-slate-200">
+                <div className=" w-[30vw] flex flex-col items-center border-2 border-solid border-gray-400 p-6 bg-slate-800">
+                    <img src="../robo-city.jpg" alt="robo-city img" className=" object-cover transition-all hover:scale-90"/>
+                    <div className="mt-3 font-bold text-xl">
+                        <Link to={'/robo-city'}>Start</Link>
+                    </div>
+                </div>
         </div>
     )
 }
