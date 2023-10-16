13e67579830aaadfd1a014e3bc916d70d09fb418
faizan-20 <faizan.20a@gmail.com>
2023-10-16 21:31:47 +0530

    changed navitems to links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 /* eslint-disable react/prop-types */
 function Navbar({ seconds, minutes }) {
     return (
@@ -5,9 +7,9 @@ function Navbar({ seconds, minutes }) {
             <div className="nav-container flex justify-evenly items-center bg-slate-900 shadow-sm shadow-slate-800 text-slate-200 text-xl h-16">
                 <div id="navHead" className=" font-bold text-2xl" >Find Everyone</div>
                 <div id="navitems" className="flex">
-                    <div className="navItem">Home</div>
-                    <div className="navItem ml-10">About</div>
-                    <div className="navItem ml-10">Leaderboard</div>
+                    <Link to="/" className="navItem text-slate-200">Home</Link>
+                    <Link to="/about" className="navItem ml-10 text-slate-200">About</Link>
+                    <Link to="/leaderboard" className="navItem ml-10 text-slate-200">Leaderboard</Link>
                 </div>
                 <div id="timer">{minutes}:{seconds}</div>
             </div>
